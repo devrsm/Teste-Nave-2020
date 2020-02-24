06c63e34caecbe2d4d74a2a402bd6c56ff7a2245
Roberto S Machado <60455148+devrsm@users.noreply.github.com>
2020-02-24 19:49:18 -0300

    Delete E11 falta

--- Teste Vaga Nave/E11 falta.js
+++
@@ -1,9 +0,0 @@
-// Ordene o array de forma decrescente por idade, em caso de empate o desempate é pelo id.
-
-const colaboradores = [
-    {id: 1, nome: 'juca', sobrenome: 'da silva', idade: 42},
-    {id: 2, nome: 'daniel', sobrenome: 'gonçalves',  idade: 21},
-    {id:3, nome: 'matheus', sobrenome: 'garcia', idade: 28},
-    {id: 4, nome: 'gabriel', sobrenome: 'pinheiro',  idade: 21}
-]
-
