e6fea37928ea72a1bcc973022046bc3bc41670fb
Roberto S Machado <60455148+devrsm@users.noreply.github.com>
2020-02-24 19:50:19 -0300

    Delete E6 falta

--- Teste Vaga Nave/E6 falta.js
+++
@@ -1,9 +0,0 @@
-// Utilizando a api da viacep (https://viacep.com.br/) e o seu cep como entrada imprima o seu endereço no formato 'ENDERECO, NUMERO, CIDADE/ESTADO'
-
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var url = "https://viacep.com.br/ws/96040420/json";
-var jhttp = new XMLHttpRequest();
-jhttp.open("GET", url, false);
-jhttp.send(null);
-
-console.log(jhttp)
